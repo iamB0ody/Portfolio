ab44ff26c5fa836d7b440b48118744c58d42ee13
unknown <abdelrahman.ui@gmail.com>
2017-10-11 11:13:55 +0200

    start psuh

---
+++ footer.php
@@ -0,0 +1,25 @@
+
+
+<footer class="footer">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12">
+				<a>From the bottom of my heart i thank you for visiting my site</a>
+			</div><!-- row -->
+	</div><!-- container-fluid downfooter -->
+</footer><!-- #footer -->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    	$(document).ready(function(){
+
+				//$('.skill').height($('.skill').width()).css({'padding':'30% 0'});
+
+    	});
+    </script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,107 @@
+<?php include 'header.php'; ?>
+
+
+
+
+
+
+
+<div class="container brief">
+	<div class="row">
+
+		<div class="col-xs-12 headofsection">
+			<h6>about</h6>
+		</div>
+
+		<div class="col-xs-12">
+			<p>
+				<b>AbdElrahman Ahmad</b>, 22 years old, student at Madina Academy, Systems and information management department, 2 years experience in user interfaces development <b>"UI Development"</b>, Lover to the field of information technology <b>"IT"</b>, have small experience with Web penetration testing in addition to that I have a lot of basics of programming.
+			</p>
+		</div><!-- col-xs-12 -->
+	</div><!-- row -->
+</div><!-- container-fluid brief -->
+
+
+<?php include 'skills.php'; ?>
+
+
+<div class="container portfolio">
+	<div class="row">
+
+		<div class="col-xs-12 headofsection">
+			<h6>Portfolio</h6>
+		</div>
+
+		<div class="col-lg-3 col-md-4 col-sm-4">
+			<div class="item">
+				<img src="img/item-bg.png" class="topimgitem" alt="">
+				<div class="itemimg">
+					<img src="img/fortest.png" alt="">
+					<div class="more">
+						<ul>
+							<a href="#Preview"><li>Preview</li></a>
+							<a href="#Source"><li>Source Code</li></a>
+						</ul>
+					</div>
+				</div>
+				<h1>name of project</h1>
+			</div><!-- item -->
+		</div><!-- col-lg-3 col-md-4 col-sm-4  -->
+
+		<div class="col-lg-3 col-md-4 col-sm-4">
+			<div class="item">
+				<img src="img/item-bg.png" class="topimgitem" alt="">
+				<div class="itemimg">
+					<img src="img/fortest.png" alt="">
+					<div class="more">
+						<ul>
+							<a href="#Preview"><li>Preview</li></a>
+							<a href="#Source"><li>Source Code</li></a>
+						</ul>
+					</div>
+				</div>
+				<h1>name of project</h1>
+			</div><!-- item -->
+		</div><!-- col-lg-3 col-md-4 col-sm-4  -->
+
+		<div class="col-lg-3 col-md-4 col-sm-4">
+			<div class="item">
+				<img src="img/item-bg.png" class="topimgitem" alt="">
+				<div class="itemimg">
+					<img src="img/fortest.png" alt="">
+					<div class="more">
+						<ul>
+							<a href="#Preview"><li>Preview</li></a>
+							<a href="#Source"><li>Source Code</li></a>
+						</ul>
+					</div>
+				</div>
+				<h1>name of project</h1>
+			</div><!-- item -->
+		</div><!-- col-lg-3 col-md-4 col-sm-4  -->
+
+		<div class="col-lg-3 col-md-4 col-sm-4">
+			<div class="item">
+				<img src="img/item-bg.png" class="topimgitem" alt="">
+				<div class="itemimg">
+					<img src="img/fortest.png" alt="">
+					<div class="more">
+						<ul>
+							<a href="#Preview"><li>Preview</li></a>
+							<a href="#Source"><li>Source Code</li></a>
+						</ul>
+					</div>
+				</div>
+				<h1>name of project</h1>
+			</div><!-- item -->
+		</div><!-- col-lg-3 col-md-4 col-sm-4  -->
+
+	</div><!-- row -->
+</div><!-- container-fluid -->
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
